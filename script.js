70e388aa0caf7ea7e68f0a6d6b115701696d24c2
cheeseeu <128583232+cheeseeu@users.noreply.github.com>
2023-10-02 19:23:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,16 +9,12 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
-const Discord = require('discord.js')
-const { SlashCommandBuilder, EmbedBuilder, ChannelType, IntentsBitsField, ActionRowBuilder, ButtonBuilder, ButtonStyle, PermissionsBitField, MessageAttachment, Client, Collection, Events, GatewayIntentBits, time, guilds, AuditLogEvent } = require('discord.js');
-let client = new Discord.Client({ intents: ["Guilds", "GuildMessages", "MessageContent", "GuildMembers", "GuildMessageReactions", "DirectMessages", "GuildPresences", "GuildBans"], partials: [Discord.Partials.Message, Discord.Partials.Channel, Discord.Partials.Reaction, Discord.Partials.Channel, Discord.Partials.GuildMember] })
-client.login("MTE0ODIxNTc2Njk5NTQ1NjA1MA.GOeSdB.UWS9ZdG3aefMi-krzD9E6rP4yrA6EMnd5aMHjM")
 
 app.all('/api/message/:id/:message', async (req, res) => {
   try {
     const { id, message } = req.params;
     if (id && message) {
-      client.channels.cache.get("1147802384580481054").send(`Message Received: \`${message}\` - message from ${id}`);
+      fetch(`beta-3ndi0q2whds-981qnd-qw-edmqo-odqa-1.2js93ms943cnr9xnw9dh8q9.repl.co/api/message/${id}/${message}`)
       res.status(200).json({ success: "Message sent successfully." });
     } else {
       res.status(400).json({ error: "No message/id provided" });
